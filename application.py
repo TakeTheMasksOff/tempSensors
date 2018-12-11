08add8a789c695807224efbc3395b83f50b75362
Ahmad Khalafbayli <akhalafbayli@rcom.az>
2018-12-11 18:06:54 +0400

    server-client works

--- application.py
+++ application.py
@@ -16,17 +16,55 @@ Updated 13th April 2018
 """
 
 
-
-
 # Start with a basic flask app webpage.
 from flask_socketio import SocketIO, emit
 from flask import Flask, render_template, url_for, copy_current_request_context
 from random import random
 from time import sleep
-from threading import Thread, Event
+# from threading import Thread, Event
+import threading
+import requests
+import datetime
+import json
 
+link_AHU1_AHU2_intake_supply_AHU1_mid = 'http://172.19.6.231/kbr/?pt=31&cmd=list'
+link_AHU4_AHU3_intake_supply = 'http://172.19.6.231/kbr/?pt=30&cmd=list'
+link_AHU2_3_4_mid = 'http://172.19.6.231/kbr/?pt=32&cmd=list'
+link_outside = "http://172.19.6.231/kbr/?pt=33&cmd=list"
 
-__author__ = 'slynn'
+map_obj = {
+    "a43f8f2f1401": "device_outside" ,
+    "aaabe9191302": "AHU1_mid" ,
+    "aaeb5c1a1302": "AHU2_mid",
+    "89ab10301401": "AHU3_mid",
+    "aa45ba1a1302": "AHU4_mid",
+    "aa16691a1302": "AHU4_supply",
+    "aa85b61a1302": "AHU4_intake",
+    "aad0cc1a1302": "AHU3_supply" ,
+    "fc546f2f1401": "AHU3_intake" ,
+    "05b910301401": "AHU2_supply" ,
+    "2ebabe2f1401": "AHU2_intake" ,
+    "dc1d532f1401": "AHU1_supply",
+    "aa89c61a1302": "AHU1_intake"
+}
+
+temps_obj = {
+    "device_outside": 1,
+    "AHU4_mid": 1,
+    "AHU4_supply": 1,
+    "AHU4_intake": 1,
+    "AHU3_mid": 1,
+    "AHU3_supply": 1,
+    "AHU3_intake": 1,
+    "AHU2_mid": 1,
+    "AHU2_supply": 1,
+    "AHU2_intake": 1,
+    "AHU1_mid": 1,
+    "AHU1_supply": 1,
+    "AHU1_intake": 1
+}
+
+__author__ = 'TakeTheMasksOff'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -35,30 +73,30 @@ app.config['DEBUG'] = True
 #turn the flask app into a socketio app
 socketio = SocketIO(app)
 
-#random number Generator Thread
-thread = Thread()
-thread_stop_event = Event()
+# #random number Generator Thread
+# thread = Thread()
+# thread_stop_event = Event()
 
-class RandomThread(Thread):
-    def __init__(self):
-        self.delay = 1
-        super(RandomThread, self).__init__()
+# class RandomThread(Thread):
+#     def __init__(self):
+#         self.delay = 1
+#         super(RandomThread, self).__init__()
 
-    def randomNumberGenerator(self):
-        """
-        Generate a random number every 1 second and emit to a socketio instance (broadcast)
-        Ideally to be run in a separate thread?
-        """
-        #infinite loop of magical random numbers
-        print("Making random numbers")
-        while not thread_stop_event.isSet():
-            number = round(random()*10, 3)
-            print(number)
-            socketio.emit('newnumber', {'number': number}, namespace='/test')
-            sleep(self.delay)
+#     def randomNumberGenerator(self):
+#         """
+#         Generate a random number every 1 second and emit to a socketio instance (broadcast)
+#         Ideally to be run in a separate thread?
+#         """
+#         #infinite loop of magical random numbers
+#         print("Making random numbers")
+#         while not thread_stop_event.isSet():
+#             number = round(random()*10, 3)
+#             print(number)
+#             socketio.emit('newnumber', {'number': number}, namespace='/test')
+#             sleep(self.delay)
 
-    def run(self):
-        self.randomNumberGenerator()
+#     def run(self):
+#         self.randomNumberGenerator()
 
 
 @app.route('/')
@@ -66,22 +104,103 @@ def index():
     #only by sending this page first will the client be connected to the socketio instance
     return render_template('index.html')
 
+
 @socketio.on('connect', namespace='/test')
 def test_connect():
     # need visibility of the global thread object
-    global thread
+    # global threading
     print('Client connected')
+    # #Start the random number generator thread only if the thread has not been started before.
+    # if not thread.isAlive():
+    #     print("Starting Thread")
+    #     thread = RandomThread()
+    #     thread.start()
 
-    #Start the random number generator thread only if the thread has not been started before.
-    if not thread.isAlive():
-        print("Starting Thread")
-        thread = RandomThread()
-        thread.start()
+    printit()
+
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
     print('Client disconnected')
 
+
+def printit():
+        threading.Timer(60, printit).start()
+        init_transfer_obj()
+        js_string = collect_data()
+        print("json obj type=", type(js_string))
+        print("json obj to send=", js_string)
+        socketio.emit('sensors', js_string, namespace='/test')
+        print("iteration end time", datetime.datetime.now().strftime("%c"))
+
+def get_raw_devices_data(link, delay, time_out):
+    if delay != 0:
+        print("starts with delay=", delay)
+        sleep(delay)
+    else:
+        print("starts without delay")
+    try:
+        r = requests.get(link, timeout=time_out)
+        return r.text
+    except requests.exceptions.Timeout:
+        print("Timeout during calling ", link)
+        return None
+
+
+def collect_data():
+    print("iteration start time", datetime.datetime.now().strftime("%c"))
+    raw_data = get_raw_devices_data(
+        link_AHU1_AHU2_intake_supply_AHU1_mid, 0, 5)
+    print("link_AHU1_AHU2_intake_supply_AHU1_mid=", raw_data)
+    if raw_data == None:
+        raw_data = get_raw_devices_data(
+            link_AHU1_AHU2_intake_supply_AHU1_mid, 0, 5)
+        print("second call link_AHU1_AHU2_intake_supply_AHU1_mid=", raw_data)
+    parse_from_device(raw_data)
+
+    raw_data = get_raw_devices_data(link_AHU4_AHU3_intake_supply, 5, 5)
+    print("link_AHU4_AHU3_intake_supply=", raw_data)
+    if raw_data == None:
+        raw_data = get_raw_devices_data(link_AHU4_AHU3_intake_supply, 5, 5)
+        print("second call link_AHU4_AHU3_intake_supply=", raw_data)
+    parse_from_device(raw_data)
+
+    raw_data = get_raw_devices_data(link_AHU2_3_4_mid, 5, 5)
+    print("link_AHU2_3_4_mid=", raw_data)
+    if raw_data == None:
+        raw_data = get_raw_devices_data(link_AHU2_3_4_mid, 5, 5)
+        print("second call link_AHU2_3_4_mid=", raw_data)
+    parse_from_device(raw_data)
+
+    raw_data = get_raw_devices_data(link_outside, 5, 5)
+    print("link_outside=", raw_data)
+    if raw_data == None:
+        raw_data = get_raw_devices_data(link_outside, 5, 5)
+        print("second call link_outside=", raw_data)
+    parse_from_device(raw_data)
+    js_string = json.dumps(temps_obj)
+    
+    return js_string
+    
+
+
+def parse_from_device(raw_data):
+    if raw_data:
+        partial_devices_data = raw_data.split(";")
+        print("separated data=", partial_devices_data)
+        for sensor_data in partial_devices_data:
+            sensor = sensor_data.split(":")
+            print("sensor=", sensor)
+            if sensor[0] in map_obj:
+                key = map_obj[sensor[0]]
+                print("key=", key)
+                if sensor[1]:
+                    temps_obj[key] = sensor[1]
+
+def init_transfer_obj():
+    for key in temps_obj:
+        temps_obj[key] = None
+    print("temps_obj=", temps_obj)
 
 if __name__ == '__main__':
     socketio.run(app)
--- static/js/application.js
+++ static/js/application.js
@@ -1,22 +1,25 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
+// document.addEventListener("load", function () {
+    
     //connect to the socket server.
     var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
     var numbers_received = [];
 
     //receive details from server
-    socket.on('newnumber', function(msg) {
-        console.log("Received number" + msg.number);
-        //maintain a list of ten numbers
-        if (numbers_received.length >= 10){
-            numbers_received.shift()
-        }            
-        numbers_received.push(msg.number);
-        numbers_string = '';
-        for (var i = 0; i < numbers_received.length; i++){
-            numbers_string = numbers_string + '<p>' + numbers_received[i].toString() + '</p>';
-        }
-        $('#log').html(numbers_string);
+    socket.on('sensors', function (msg) {
+        var parsed_obj = JSON.parse(msg)
+        console.log(DateT);
+        // get a new date (locale machine date time)
+        var date = new Date();
+        // get the date as a string
+        var n = date.toDateString();
+        // get the time as a string
+        var time = date.toLocaleTimeString();
+
+        // log the date in the browser console
+        console.log('date & time:', n, " ", time);
+        console.log(parsed_obj);
     });
 
 });
