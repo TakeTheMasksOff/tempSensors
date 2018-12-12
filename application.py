da40eaee01d22514a7d74c7931cbf9ad60da5a61
Ahmad Khalafbayli <akhalafbayli@rcom.az>
2018-12-12 16:00:26 +0400

    version 0.1

--- application.py
+++ application.py
@@ -1,27 +1,6 @@
-"""
-Demo Flask application to test the operation of Flask with socket.io
-
-Aim is to create a webpage that is constantly updated with random numbers from a background python process.
-
-30th May 2014
-
-===================
-
-Updated 13th April 2018
-
-+ Upgraded code to Python 3
-+ Used Python3 SocketIO implementation
-+ Updated CDN Javascript and CSS sources
-
-"""
-
-
-# Start with a basic flask app webpage.
 from flask_socketio import SocketIO, emit
 from flask import Flask, render_template, url_for, copy_current_request_context
-from random import random
 from time import sleep
-# from threading import Thread, Event
 import threading
 import requests
 import datetime
@@ -29,21 +8,23 @@ import json
 
 link_AHU1_AHU2_intake_supply_AHU1_mid = 'http://172.19.6.231/kbr/?pt=31&cmd=list'
 link_AHU4_AHU3_intake_supply = 'http://172.19.6.231/kbr/?pt=30&cmd=list'
-link_AHU2_3_4_mid = 'http://172.19.6.231/kbr/?pt=32&cmd=list'
+link_AHU4_mid = 'http://172.19.6.231/kbr/?pt=32&cmd=list'
+link_AHU2_mid = 'http://172.19.6.231/kbr/?pt=34&cmd=list'
+link_AHU3_mid = 'http://172.19.6.231/kbr/?pt=35&cmd=list'
 link_outside = "http://172.19.6.231/kbr/?pt=33&cmd=list"
 
 map_obj = {
-    "a43f8f2f1401": "device_outside" ,
-    "aaabe9191302": "AHU1_mid" ,
+    "a43f8f2f1401": "device_outside",
+    "aaabe9191302": "AHU1_mid",
     "aaeb5c1a1302": "AHU2_mid",
     "89ab10301401": "AHU3_mid",
     "aa45ba1a1302": "AHU4_mid",
     "aa16691a1302": "AHU4_supply",
     "aa85b61a1302": "AHU4_intake",
-    "aad0cc1a1302": "AHU3_supply" ,
-    "fc546f2f1401": "AHU3_intake" ,
-    "05b910301401": "AHU2_supply" ,
-    "2ebabe2f1401": "AHU2_intake" ,
+    "aad0cc1a1302": "AHU3_supply",
+    "fc546f2f1401": "AHU3_intake",
+    "05b910301401": "AHU2_supply",
+    "2ebabe2f1401": "AHU2_intake",
     "dc1d532f1401": "AHU1_supply",
     "aa89c61a1302": "AHU1_intake"
 }
@@ -64,74 +45,114 @@ temps_obj = {
     "AHU1_intake": 1
 }
 
+
 __author__ = 'TakeTheMasksOff'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.config['DEBUG'] = True
 
-#turn the flask app into a socketio app
+# turn the flask app into a socketio app
 socketio = SocketIO(app)
+client_count = 0
 
-# #random number Generator Thread
-# thread = Thread()
-# thread_stop_event = Event()
-
-# class RandomThread(Thread):
-#     def __init__(self):
-#         self.delay = 1
-#         super(RandomThread, self).__init__()
-
-#     def randomNumberGenerator(self):
-#         """
-#         Generate a random number every 1 second and emit to a socketio instance (broadcast)
-#         Ideally to be run in a separate thread?
-#         """
-#         #infinite loop of magical random numbers
-#         print("Making random numbers")
-#         while not thread_stop_event.isSet():
-#             number = round(random()*10, 3)
-#             print(number)
-#             socketio.emit('newnumber', {'number': number}, namespace='/test')
-#             sleep(self.delay)
-
-#     def run(self):
-#         self.randomNumberGenerator()
+@app.before_first_request
+def start_requesting_sensors():
+    
+    print("client_count=", client_count)
+    print("############################# STARTING SCHEDULE  #############################")
+    my_schedule()
 
 
 @app.route('/')
 def index():
-    #only by sending this page first will the client be connected to the socketio instance
+    # only by sending this page first will the client be connected to the socketio instance
     return render_template('index.html')
 
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
-    # need visibility of the global thread object
-    # global threading
-    print('Client connected')
-    # #Start the random number generator thread only if the thread has not been started before.
-    # if not thread.isAlive():
-    #     print("Starting Thread")
-    #     thread = RandomThread()
-    #     thread.start()
-
-    printit()
+    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^Client connected^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+    print("connection time", datetime.datetime.now().strftime("%c"))
+    global client_count
+    client_count += 1
+    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^Total client count = ', client_count)
+    
 
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
-    print('Client disconnected')
+    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^Client disconnected^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+    print("disconnection time", datetime.datetime.now().strftime("%c"))
+    global client_count
+    client_count -= 1
+    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^Total client count = ', client_count)
 
 
-def printit():
-        threading.Timer(60, printit).start()
-        init_transfer_obj()
-        js_string = collect_data()
-        print("json obj type=", type(js_string))
-        print("json obj to send=", js_string)
-        socketio.emit('sensors', js_string, namespace='/test')
-        print("iteration end time", datetime.datetime.now().strftime("%c"))
+def my_schedule():
+    print("*********************************")
+    print("iteration start time", datetime.datetime.now().strftime("%c"))
+    init_transfer_obj()
+    js_string = collect_data()
+    print("json obj to send=", js_string)
+    socketio.emit('sensors', js_string, namespace='/test')
+    print("iteration end time", datetime.datetime.now().strftime("%c"))
+    print("=================================")
+    threading.Timer(30, my_schedule).start()
+
+
+def init_transfer_obj():
+    for key in temps_obj:
+        temps_obj[key] = None
+    print("temps_obj=", temps_obj)
+
+
+def collect_data():
+    raw_data = get_raw_devices_data(link_AHU1_AHU2_intake_supply_AHU1_mid, 0, 5)
+    print("link_AHU1_AHU2_intake_supply_AHU1_mid=", raw_data)
+    if raw_data == None:
+        raw_data = get_raw_devices_data(link_AHU1_AHU2_intake_supply_AHU1_mid, 5, 5)
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  second call link_AHU1_AHU2_intake_supply_AHU1_mid=", raw_data)
+    parse_from_device(raw_data)
+
+    raw_data = get_raw_devices_data(link_AHU4_AHU3_intake_supply, 2, 5)
+    print("link_AHU4_AHU3_intake_supply=", raw_data)
+    if raw_data == None:
+        raw_data = get_raw_devices_data(link_AHU4_AHU3_intake_supply, 5, 5)
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  second call link_AHU4_AHU3_intake_supply=", raw_data)
+    parse_from_device(raw_data)
+
+    raw_data = get_raw_devices_data(link_AHU4_mid, 2, 5)
+    print("link_AHU4_mid=", raw_data)
+    if raw_data == None:
+        raw_data = get_raw_devices_data(link_AHU4_mid, 5, 5)
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  second call link_AHU4_mid=", raw_data)
+    parse_from_device(raw_data)
+
+    raw_data = get_raw_devices_data(link_AHU3_mid, 2, 5)
+    print("link_AHU3_mid=", raw_data)
+    if raw_data == None:
+        raw_data = get_raw_devices_data(link_AHU3_mid, 5, 5)
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  second call link_AHU3_mid=", raw_data)
+    parse_from_device(raw_data)
+
+    raw_data = get_raw_devices_data(link_AHU2_mid, 2, 5)
+    print("link_AHU2_mid=", raw_data)
+    if raw_data == None:
+        raw_data = get_raw_devices_data(link_AHU2_mid, 5, 5)
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  second call link_AHU2_mid=", raw_data)
+    parse_from_device(raw_data)
+
+    raw_data = get_raw_devices_data(link_outside, 2, 5)
+    print("link_outside=", raw_data)
+    if raw_data == None:
+        raw_data = get_raw_devices_data(link_outside, 5, 5)
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  second call link_outside=", raw_data)
+    parse_from_device(raw_data)
+    js_string = json.dumps(temps_obj)
+
+    return js_string
+
 
 def get_raw_devices_data(link, delay, time_out):
     if delay != 0:
@@ -147,43 +168,6 @@ def get_raw_devices_data(link, delay, time_out):
         return None
 
 
-def collect_data():
-    print("iteration start time", datetime.datetime.now().strftime("%c"))
-    raw_data = get_raw_devices_data(
-        link_AHU1_AHU2_intake_supply_AHU1_mid, 0, 5)
-    print("link_AHU1_AHU2_intake_supply_AHU1_mid=", raw_data)
-    if raw_data == None:
-        raw_data = get_raw_devices_data(
-            link_AHU1_AHU2_intake_supply_AHU1_mid, 0, 5)
-        print("second call link_AHU1_AHU2_intake_supply_AHU1_mid=", raw_data)
-    parse_from_device(raw_data)
-
-    raw_data = get_raw_devices_data(link_AHU4_AHU3_intake_supply, 5, 5)
-    print("link_AHU4_AHU3_intake_supply=", raw_data)
-    if raw_data == None:
-        raw_data = get_raw_devices_data(link_AHU4_AHU3_intake_supply, 5, 5)
-        print("second call link_AHU4_AHU3_intake_supply=", raw_data)
-    parse_from_device(raw_data)
-
-    raw_data = get_raw_devices_data(link_AHU2_3_4_mid, 5, 5)
-    print("link_AHU2_3_4_mid=", raw_data)
-    if raw_data == None:
-        raw_data = get_raw_devices_data(link_AHU2_3_4_mid, 5, 5)
-        print("second call link_AHU2_3_4_mid=", raw_data)
-    parse_from_device(raw_data)
-
-    raw_data = get_raw_devices_data(link_outside, 5, 5)
-    print("link_outside=", raw_data)
-    if raw_data == None:
-        raw_data = get_raw_devices_data(link_outside, 5, 5)
-        print("second call link_outside=", raw_data)
-    parse_from_device(raw_data)
-    js_string = json.dumps(temps_obj)
-    
-    return js_string
-    
-
-
 def parse_from_device(raw_data):
     if raw_data:
         partial_devices_data = raw_data.split(";")
@@ -197,10 +181,6 @@ def parse_from_device(raw_data):
                 if sensor[1]:
                     temps_obj[key] = sensor[1]
 
-def init_transfer_obj():
-    for key in temps_obj:
-        temps_obj[key] = None
-    print("temps_obj=", temps_obj)
 
 if __name__ == '__main__':
     socketio.run(app)
--- static/js/application.js
+++ static/js/application.js
@@ -9,7 +9,6 @@ $(document).ready(function () {
     //receive details from server
     socket.on('sensors', function (msg) {
         var parsed_obj = JSON.parse(msg)
-        console.log(DateT);
         // get a new date (locale machine date time)
         var date = new Date();
         // get the date as a string
@@ -18,8 +17,119 @@ $(document).ready(function () {
         var time = date.toLocaleTimeString();
 
         // log the date in the browser console
+        console.log("****************************")
         console.log('date & time:', n, " ", time);
         console.log(parsed_obj);
+        console.log("============================")
+        $(".last-update").html('Last update time: ' + n + " " + time)
+
+        if (parsed_obj.AHU1_intake) {
+            $(".device .in-value .value").removeClass("warning");
+            $(".device .in-value .value").html(parsed_obj.AHU1_intake);
+        }else {
+            $(".device .in-value .value").addClass("warning");
+            $(".device .in-value .value").html("X");
+        }
+
+        if (parsed_obj.AHU1_supply) {
+            $(".device .out-value .value").removeClass("warning");
+            $(".device .out-value .value").html(parsed_obj.AHU1_supply);
+        }else {
+            $(".device .out-value .value").addClass("warning");
+            $(".device .out-value .value").html("X");
+        }
+
+        if (parsed_obj.AHU1_mid) {
+            $(".device .middle .round-border").removeClass("warning");
+            $(".device .middle .round-border").html(parsed_obj.AHU1_mid);
+        }else {
+            $(".device .middle .round-border").addClass("warning");
+            $(".device .middle .round-border").html("X");
+        }
+
+
+
+        if (parsed_obj.AHU2_intake) {
+            $(".device-2 .in-value .value").removeClass("warning");
+            $(".device-2 .in-value .value").html(parsed_obj.AHU2_intake);
+        }else {
+            $(".device-2 .in-value .value").addClass("warning");
+            $(".device-2 .in-value .value").html("X");
+        }
+
+        if (parsed_obj.AHU2_supply) {
+            $(".device-2 .out-value .value").removeClass("warning");
+            $(".device-2 .out-value .value").html(parsed_obj.AHU2_supply);
+        }else {
+            $(".device-2 .out-value .value").addClass("warning");
+            $(".device-2 .out-value .value").html("X");
+        }
+
+        if (parsed_obj.AHU2_mid) {
+            $(".device-2 .middle .round-border").removeClass("warning");
+            $(".device-2 .middle .round-border").html(parsed_obj.AHU2_mid);
+        }else {
+            $(".device-2 .middle .round-border").addClass("warning");
+            $(".device-2 .middle .round-border").html("X");
+        }
+
+
+        if (parsed_obj.AHU3_intake) {
+            $(".device-3 .in-value .value").removeClass("warning");
+            $(".device-3 .in-value .value").html(parsed_obj.AHU3_intake);
+        }else {
+            $(".device-3 .in-value .value").addClass("warning");
+            $(".device-3 .in-value .value").html("X");
+        }
+
+        if (parsed_obj.AHU3_supply) {
+            $(".device-3 .out-value .value").removeClass("warning");
+            $(".device-3 .out-value .value").html(parsed_obj.AHU3_supply);
+        }else {
+            $(".device-3 .out-value .value").addClass("warning");
+            $(".device-3 .out-value .value").html("X");
+        }
+
+        if (parsed_obj.AHU3_mid) {
+            $(".device-3 .middle .round-border").removeClass("warning");
+            $(".device-3 .middle .round-border").html(parsed_obj.AHU3_mid);
+        }else {
+            $(".device-3 .middle .round-border").addClass("warning");
+            $(".device-3 .middle .round-border").html("X");
+        }
+
+
+        if (parsed_obj.AHU4_intake) {
+            $(".device-4 .in-value .value").removeClass("warning");
+            $(".device-4 .in-value .value").html(parsed_obj.AHU4_intake);
+        } else {
+            $(".device-4 .in-value .value").addClass("warning");
+            $(".device-4 .in-value .value").html("X");
+        }
+
+        if (parsed_obj.AHU4_supply) {
+            $(".device-4 .out-value .value").removeClass("warning");
+            $(".device-4 .out-value .value").html(parsed_obj.AHU4_supply);
+        } else {
+            $(".device-4 .out-value .value").addClass("warning");
+            $(".device-4 .out-value .value").html("X");
+        }
+
+        if (parsed_obj.AHU4_mid) {
+            $(".device-4 .middle .round-border").removeClass("warning");
+            $(".device-4 .middle .round-border").html(parsed_obj.AHU4_mid);
+        } else {
+            $(".device-4 .middle .round-border").addClass("warning");
+            $(".device-4 .middle .round-border").html("X");
+        }
+
+        if (parsed_obj.device_outside) {
+            $(".outside-temp").removeClass("warning");
+            $(".outside-temp").html(parsed_obj.device_outside);
+        } else {
+            $(".outside-temp").addClass("warning");
+            $(".outside-temp").html("X");
+        }
     });
 
 });
